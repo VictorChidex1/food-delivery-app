96583b693e907048cd57575399809c0fc1b7ff08
“VictorChidex1” <Donchid.online@gmail.com>
2025-12-20 02:04:34 +0100

    fix(auth): enforce browserLocalPersistence

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,9 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import {
+  getAuth,
+  setPersistence,
+  browserLocalPersistence,
+} from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
@@ -16,6 +20,12 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+
+// Enforce Local Persistence
+setPersistence(auth, browserLocalPersistence).catch((error) => {
+  console.error("Firebase Persistence Error:", error);
+});
+
 export const db = getFirestore(app);
 
 export default app;
